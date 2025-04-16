c5ecdde59851f7c459e2b27af6c946d4d7dc019b
Zair <zairabbas1533@outlook.com>
2025-04-16 17:07:54 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl font-bold font-circular-web">
+        Welcome to Awwwards
+      </h1>
+    </main>
+  );
+};
+
+export default App;
